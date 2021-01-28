356b46a8ab5897555dc79cab68d7b33b981cadfd
Shashwat Kashyap <shashwat.kashyap2@gmail.com>
2021-01-28 15:45:29 +0530

    StormnViewer Completed

---
+++ StormViewer/SourceViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SourceViewController.swift
+//  StormViewer
+//
+//  Created by Shashwat Kashyap on 28/01/21.
+//  Copyright © 2021 Shashwat Kashyap. All rights reserved.
+//
+
+import Cocoa
+
+class SourceViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
